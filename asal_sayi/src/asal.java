f40eced08c9e988d0311ec771a3c4cb62869ba3d
Enes <95242303+Enesderin0@users.noreply.github.com>
2022-04-18 15:28:53 +0300

    asal sayılar

---
+++ asal_sayi/src/asal.java
@@ -0,0 +1,9 @@
+public class asal {
+    public static void main(String[] args) {
+        for (int i=1;i<=100;i++){
+            if (i%2!=0 && i%3!=0 && i%4!=0 && i%5!=0 && i%6!=0 &&i%7!=0 && i%8!=0 && i%9!=0){
+                System.out.println(i);
+            }
+        }
+    }
+}
